c6444218b7f167aa509b5f9bb98c7e7448a0ec7b
Razvan <oddbit.studio@gmail.com>
2021-11-30 13:00:37 +0200

    Create schemas.py

---
+++ app/api/schemas.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel, validator
+from enum import IntEnum
+import datetime
+
+
+class Slot(IntEnum):
+    midnight_to_eight = 1
+    eight_to_four = 2
+    four_to_midnight = 3
+
+
+class Worker(BaseModel):
+    id: int
+    name: str
+
+    @validator('name', pre=True)
+    def blank_string(value, field):
+        if value == "":
+            return None
+        return value
+
+    class Config:
+        orm_mode = True
+
+
+class ShiftResponse(BaseModel):
+    slot: Slot
+    date: datetime.date
+
+    class Config:
+        use_enum_vallues = True
+
+class ShiftCreate(ShiftResponse):
+    worker_id: int
+
+
+class Shift(ShiftCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
